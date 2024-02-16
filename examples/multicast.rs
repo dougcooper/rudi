fa9c3c7b8fcf549d97ea2c34c2f630d418fb84fc
Douglas.A.Cooper <douglas.a.cooper.civ@us.navy.mil>
2024-02-16 07:09:45 -0500

    multicast example

---
+++ examples/multicast.rs
@@ -0,0 +1,29 @@
+use std::net::{Ipv4Addr, SocketAddrV4};
+use rudi::{udpmanager::UdpManager, CastMode, IpConfigV4};
+
+async fn recv_data(rx: &mut tokio::sync::broadcast::Receiver<Vec<u8>>) {
+    while let Ok(data) = rx.recv().await {
+        println!("received {} bytes of data", data.len());
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let mut udp = UdpManager::default();
+    let unicast = IpConfigV4 {
+        cast_mode: CastMode::Multicast("224.1.1.100".parse::<Ipv4Addr>().unwrap()),
+        addr: "0.0.0.0:6993".parse::<SocketAddrV4>().unwrap(),
+    };
+    let mut rx1 = udp.subscribe(&unicast).await.unwrap();
+
+    let mut ctrlc = tokio::spawn(tokio::signal::ctrl_c());
+
+    loop {
+        tokio::select! {
+            _ = &mut ctrlc => break,
+            _ = recv_data(&mut rx1) => {}
+        }
+    }
+
+    println!("ctrlc called. exiting...");
+}
